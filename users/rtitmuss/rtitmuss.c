72edff7780f2c28ac0679315f4dd536cc59eecb8
Richard Titmuss <richardt@spotify.com>
2020-05-04 20:21:39 +0200

    Add rtitmuss user code

---
+++ users/rtitmuss/rtitmuss.c
@@ -0,0 +1,38 @@
+#include QMK_KEYBOARD_H
+#include <rtitmuss.h>
+
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
+
+__attribute__ ((weak))
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case ALT_TAB:
+      if (record->event.pressed) {
+        if (!is_alt_tab_active) {
+          is_alt_tab_active = true;
+          register_code(KC_LGUI);
+        }
+        alt_tab_timer = timer_read();
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+      }
+      break;
+  }
+
+  return process_record_keymap(keycode, record);
+}
+
+void matrix_scan_user(void) {
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 1000) {
+      unregister_code(KC_LGUI);
+      is_alt_tab_active = false;
+    }
+  }
+}
